887f412c26d94123c628358280e423838d254d6c
pranav more <pranavmore.psm@gmail.com>
2022-05-04 18:09:23 +0530

    text badge added

---
+++ src/components/Badge/NewBadge.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="flex flex-wrap items-center">
+    <span v-for="(item, index) in itmes" :key="index">
+        <div class="flex space-x-2 ">
+            <div :class="`${item.headingSize}  text-gray-700 font-medium`">Heading</div>
+      <div
+        :class="`${item.pad} ${item.textSize} rounded-md mr-4 font-normal ${item.textColor} ${item.hoverTextColor} ${item.border} ${item.hoverlink} ${item.hoverBgColor}  ${item.bgColor}`"
+      >
+      {{ item.title }}
+      </div>
+        </div>
+    </span>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      itmes: [
+        {
+          id: 1,
+          title: "New",
+          headingSize:"text-sm",
+          textColor: "text-white",
+          pad:"py-0.5 px-2",
+          textSize:"text-xxs",
+          bgColor: "bg-gray-700",
+          hoverTextColor: "",
+          hoverBgColor: "",
+        },
+        {
+          id: 2,
+          title: "New",
+          headingSize:"text-base",
+          textColor: "text-white",
+          pad:"py-0.5 px-2",
+          textSize:"text-xxs",
+          bgColor: "bg-gray-700",
+          hoverTextColor: "",
+          hoverBgColor: "",
+        },
+
+      ],
+    };
+  },
+};
+</script>
+
+<style></style>
--- src/pages/badge.vue
+++ src/pages/badge.vue
@@ -3,132 +3,161 @@
     <div class="flex p-4">
       <!-- content -->
       <div class="flex h-screen px-4 overflow-y-hidden scrollBar">
-          <div class="overflow-y-auto px-4">
-            <Heading />
-            <div>
-          <p class="text-base font-primary">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Molestias optio quam architecto culpa unde accusamus illo error neque explicabo, numquam ut, qui eligendi magnam non quis fugiat impedit dolore aliquam?</p>
-            </div>
-             <!-- Default badge -->
-            <SubHeading > 
-                <template v-slot:subHeader>Default badge</template>
-            </SubHeading>
-            <ComponentPad>
-              <template>
-                <DefaultBadge />
-              </template>
-            </ComponentPad>
-            <!-- end default badge -->
-             <!-- Larger badge -->
-            <SubHeading > 
-                <template v-slot:subHeader>
-                    Larger badge
-                </template>
-            </SubHeading>
-            <ComponentPad>
-              <template>
-                <LargerBadge />
-              </template>
-            </ComponentPad>
-            <!-- end Larger badge -->
-             <!-- state color badge -->
-            <SubHeading > 
-                <template v-slot:subHeader>
-                    Badge State color
-                </template>
-            </SubHeading>
-            <ComponentPad>
-              <template>
-                <BadgeState />
-              </template>
-            </ComponentPad>
-            <!-- end state color badge -->
-             <!-- Outline badge -->
-            <SubHeading > 
-                <template v-slot:subHeader>
-                    Outline badge
-                </template>
-            </SubHeading>
-            <ComponentPad>
-              <template>
-                <OutlineBadge />
-              </template>
-            </ComponentPad>
-            <!-- end outline badge -->
-             <!-- Pill badge -->
-            <SubHeading > 
-                <template v-slot:subHeader>
-                    Pill badge 
-                </template>
-            </SubHeading>
-            <ComponentPad>
-              <template>
-                <PillBadge />
-              </template>
-            </ComponentPad>
-            <!-- end Pill badge -->
-             <!-- Pill outline badge -->
-            <SubHeading > 
-                <template v-slot:subHeader>
-                    Pill Outline badge 
-                </template>
-            </SubHeading>
-            <ComponentPad>
-              <template>
-                <PillOutlineBadge />
-              </template>
-            </ComponentPad>
-            <!-- end Pill outline badge -->
-             <!-- Rectange badge -->
-            <SubHeading > 
-                <template v-slot:subHeader>
-                    Rectangle badge 
-                </template>
-            </SubHeading>
-            <ComponentPad>
-              <template>
-                <RectangleBadge />
-              </template>
-            </ComponentPad>
-            <!-- end Rectangle badge -->
-             <!-- Cancel badge -->
-            <SubHeading > 
-                <template v-slot:subHeader>
-                    Badge with cancel button
-                </template>
-            </SubHeading>
-            <ComponentPad>
-              <template>
-                <CancelBadge />
-              </template>
-            </ComponentPad>
-            <!-- end Cancel badge -->
-             <!-- Link badge -->
-            <SubHeading > 
-                <template v-slot:subHeader>
-                    Badges as link
-                </template>
-            </SubHeading>
-            <ComponentPad>
-              <template>
-                <LinkBadge />
-              </template>
-            </ComponentPad>
-            <!-- end Link badge -->
-             <!-- Position Badge -->
-            <SubHeading > 
-                <template v-slot:subHeader>
-                    Positioned badge
-                </template>
-            </SubHeading>
-            <ComponentPad>
-              <template>
-                <PositionBadge />
-              </template>
-            </ComponentPad>
-            <!-- end Position Badge -->
-        
-            
+        <div class="overflow-y-auto px-4">
+          <Heading />
+          <div>
+            <p class="text-base font-primary">
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+              Molestias optio quam architecto culpa unde accusamus illo error
+              neque explicabo, numquam ut, qui eligendi magnam non quis fugiat
+              impedit dolore aliquam?
+            </p>
           </div>
+          <!-- Default badge -->
+          <SubHeading>
+            <template v-slot:subHeader
+              >Default badge</template
+            >
+          </SubHeading>
+          <ComponentPad>
+            <template>
+              <DefaultBadge />
+            </template>
+          </ComponentPad>
+          <!-- end default badge -->
+          <!-- Larger badge -->
+          <SubHeading>
+            <template v-slot:subHeader>
+              Larger badge
+            </template>
+          </SubHeading>
+          <ComponentPad>
+            <template>
+              <LargerBadge />
+            </template>
+          </ComponentPad>
+          <!-- end Larger badge -->
+          <!-- state color badge -->
+          <SubHeading>
+            <template v-slot:subHeader>
+              Badge State color
+            </template>
+          </SubHeading>
+          <ComponentPad>
+            <template>
+              <BadgeState />
+            </template>
+          </ComponentPad>
+          <!-- end state color badge -->
+          <!-- Outline badge -->
+          <SubHeading>
+            <template v-slot:subHeader>
+              Outline badge
+            </template>
+          </SubHeading>
+          <ComponentPad>
+            <template>
+              <OutlineBadge />
+            </template>
+          </ComponentPad>
+          <!-- end outline badge -->
+          <!-- Pill badge -->
+          <SubHeading>
+            <template v-slot:subHeader>
+              Pill badge
+            </template>
+          </SubHeading>
+          <ComponentPad>
+            <template>
+              <PillBadge />
+            </template>
+          </ComponentPad>
+          <!-- end Pill badge -->
+          <!-- Pill outline badge -->
+          <SubHeading>
+            <template v-slot:subHeader>
+              Pill Outline badge
+            </template>
+          </SubHeading>
+          <ComponentPad>
+            <template>
+              <PillOutlineBadge />
+            </template>
+          </ComponentPad>
+          <!-- end Pill outline badge -->
+          <!-- Rectange badge -->
+          <SubHeading>
+            <template v-slot:subHeader>
+              Rectangle badge
+            </template>
+          </SubHeading>
+          <ComponentPad>
+            <template>
+              <RectangleBadge />
+            </template>
+          </ComponentPad>
+          <!-- end Rectangle badge -->
+          <!-- Cancel badge -->
+          <SubHeading>
+            <template v-slot:subHeader>
+              Badge with cancel button
+            </template>
+          </SubHeading>
+          <ComponentPad>
+            <template>
+              <CancelBadge />
+            </template>
+          </ComponentPad>
+          <!-- end Cancel badge -->
+          <!-- Link badge -->
+          <SubHeading>
+            <template v-slot:subHeader>
+              Badges as link
+            </template>
+          </SubHeading>
+          <ComponentPad>
+            <template>
+              <LinkBadge />
+            </template>
+          </ComponentPad>
+          <!-- end Link badge -->
+          <!--  badge Sizes -->
+          <SubHeading>
+            <template v-slot:subHeader>
+              Badges as Sizes
+            </template>
+          </SubHeading>
+          <ComponentPad>
+            <template>
+              <BadgeSizes />
+            </template>
+          </ComponentPad>
+          <!-- end  badge Sizes -->
+          <!-- Position Badge -->
+          <SubHeading>
+            <template v-slot:subHeader>
+              Positioned badge
+            </template>
+          </SubHeading>
+          <ComponentPad>
+            <template>
+              <PositionBadge />
+            </template>
+          </ComponentPad>
+          <!-- end Position Badge -->
+          <!-- Text Badge -->
+          <SubHeading>
+            <template v-slot:subHeader>
+              Text badge
+            </template>
+          </SubHeading>
+          <ComponentPad>
+            <template>
+              <NewBadge />
+            </template>
+          </ComponentPad>
+          <!-- end New Badge -->
+        </div>
       </div>
       <!-- right sidebar -->
       <div class="hidden lg:block">
@@ -151,12 +180,14 @@ import CancelBadge from "../components/Badge/CancelBadge.vue";
 import LinkBadge from "../components/Badge/LinkBadge.vue";
 import PositionBadge from "../components/Badge/PositionBadge.vue";
 import PillBadge from "../components/Badge/PillBadge.vue";
+import NewBadge from "../components/Badge/NewBadge.vue";
+import BadgeSizes from "../components/Badge/BadgeSizes.vue";
 import PillOutlineBadge from "../components/Badge/PillOutlineBadge.vue";
 import Heading from "../components/Headers/Heading.vue";
-import SubHeading from '../components/Headers/SubHeading.vue';
+import SubHeading from "../components/Headers/SubHeading.vue";
 import ComponentPad from "../components/Utils/ComponentPad.vue";
-import OutlineBadge from '../components/Badge/OutlineBadge.vue';
-import RectangleBadge from '../components/Badge/RectangleBadge.vue';
+import OutlineBadge from "../components/Badge/OutlineBadge.vue";
+import RectangleBadge from "../components/Badge/RectangleBadge.vue";
 export default {
   metaInfo: {
     title: "Badges",
@@ -176,7 +207,8 @@ export default {
     LinkBadge,
     RectangleBadge,
     PositionBadge,
-
+    BadgeSizes,
+    NewBadge,
   },
 };
 </script>
