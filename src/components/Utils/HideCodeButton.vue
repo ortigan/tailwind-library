9106931ee3d0a3d5368f5f411ebf5af778ed3fd2
pranav more <pranavmore.psm@gmail.com>
2022-06-18 17:49:33 +0530

    dark mode added for default sidebar component

--- src/components/Utils/HideCodeButton.vue
+++ src/components/Utils/HideCodeButton.vue
@@ -1,4 +1,6 @@
 <template>
+<div class="cursor-pointer">
+
   <div class="flex items-center"> <span class="text-sm">Hide code</span>
     <svg width="24" height="24" stroke-width="1.5" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
 <path d="M3 3L21 21" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"/>
@@ -9,6 +11,7 @@
 
 
   </div>
+</div>
 </template>
 
 <script>
--- src/layouts/Default.vue
+++ src/layouts/Default.vue
@@ -1,14 +1,14 @@
 <template>
   <div :class="getTheme == 'light' ? '' : 'dark'">
     <Header />
-    <div class="dark:bg-gray-900 dark:text-white flex h-screen">
+    <div class="dark:bg-gray-900 dark:text-gray-100 flex h-screen">
       <div class="container  h-screen">
         <div class="flex w-full flex-col h-screen">
           <div class="flex w-full h-screen">
             <!-- sidebar left -->
             <div class="w-44 flex flex-col h-screen overflow-y-hidden">
               <div class="overflow-y-auto  hidden md:block">
-                <div class="flex flex-col mt-6 space-y-1 text-slate-700 dark:text-gray-100">
+                <div class="flex flex-col mt-6 space-y-1 text-slate-700 dark:text-gray-200">
                   <span v-for="(item, index) in menu" :key="index">
                     <div
                       :class="
