91f5ad109883b5a0dac71e0b98bca6aa2eeb3bed
pranav more <pranavmore.psm@gmail.com>
2022-06-20 13:43:47 +0530

    code snippet added for pagination component

--- src/components/Pagination/DefaultPagination.vue
+++ src/components/Pagination/DefaultPagination.vue
@@ -1,14 +1,14 @@
 <template>
   <div class="w-full text-gray-500">
     <div class="">
-        <ul class=" bg-white inline-flex -space-x-px font-medium">
-            <li class="px-3 py-2 border hover:text-gray-700 hover:cursor-pointer hover:bg-gray-50 border-gray-200 rounded-l-lg">Previous</li>
-            <li class="px-3 py-2 border hover:text-gray-700 hover:cursor-pointer hover:bg-gray-50 border-gray-200 ">1</li>
-            <li class="px-3 py-2 border hover:text-gray-700 hover:cursor-pointer hover:bg-gray-50 border-gray-200 ">2</li>
-            <li class="px-3 py-2 border text-blue-500 hover:text-blue-700 hover:cursor-pointer bg-blue-50 hover:bg-blue-100 border-gray-200">3</li>
-            <li class="px-3 py-2 border hover:text-gray-700 hover:cursor-pointer hover:bg-gray-50 border-gray-200 ">4</li>
-            <li class="px-3 py-2 border hover:text-gray-700 hover:cursor-pointer hover:bg-gray-50 border-gray-200 ">5</li>
-            <li class="px-3 py-2 border hover:text-gray-700 hover:cursor-pointer hover:bg-gray-50 border-gray-200  rounded-r-lg">Next</li>
+        <ul class="inline-flex -space-x-px font-medium">
+            <li class="px-3 py-2 border hover:text-gray-700 dark:text-gray-400 dark:bg-gray-800 hover:cursor-pointer hover:bg-gray-50 border-gray-200 dark:hover:bg-gray-700 dark:border-gray-700 rounded-l-lg">Previous</li>
+            <li class="px-3 py-2 border hover:text-gray-700 dark:text-gray-400 dark:bg-gray-800 hover:cursor-pointer hover:bg-gray-50 border-gray-200 dark:hover:bg-gray-700 dark:border-gray-700 ">1</li>
+            <li class="px-3 py-2 border hover:text-gray-700 dark:text-gray-400 dark:bg-gray-800 hover:cursor-pointer hover:bg-gray-50 border-gray-200 dark:hover:bg-gray-700 dark:border-gray-700 ">2</li>
+            <li class="px-3 py-2 border text-blue-500 hover:text-blue-700 hover:cursor-pointer bg-blue-50 hover:bg-blue-100 border-gray-200 dark:bg-gray-700 dark:text-blue-700 dark:hover:bg-gray-700 dark:border-gray-700">3</li>
+            <li class="px-3 py-2 border hover:text-gray-700 dark:text-gray-400 dark:bg-gray-800 hover:cursor-pointer hover:bg-gray-50 border-gray-200 dark:hover:bg-gray-700 dark:border-gray-700 ">4</li>
+            <li class="px-3 py-2 border hover:text-gray-700 dark:text-gray-400 dark:bg-gray-800 hover:cursor-pointer hover:bg-gray-50 border-gray-200 dark:hover:bg-gray-700 dark:border-gray-700 ">5</li>
+            <li class="px-3 py-2 border hover:text-gray-700 dark:text-gray-400 dark:bg-gray-800 hover:cursor-pointer hover:bg-gray-50 border-gray-200 dark:hover:bg-gray-700 dark:border-gray-700  rounded-r-lg">Next</li>
         </ul>
     </div>
   </div>
