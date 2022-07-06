b226c79ba2f3bf1179aab5a163c1952882fd97c1
Nandini Joshi <nandinijoshi1211@gmail.com>
2022-07-06 16:02:08 +0530

    code snippets and show/hide are done for all components including typography, excluding dropdown and accordion since the inclusion of some javascript.

--- src/components/Typography/Code Snippets/TextDecorationCodeSnippet.vue
+++ src/components/Typography/Code Snippets/TextDecorationCodeSnippet.vue
@@ -1,5 +1,26 @@
 <template>
   <pre><code class="language-markup"><script type="prism-html-markup">
+        <!--  -->
+        <div class="font flex w-full items-center justify-start space-x-2 text-gray-700">
+          <div class="text-lg font-medium overline">This is overline!</div>
+          <div class="text-xxs font-normal text-sky-400">overline</div>
+        </div>
+        <!--  -->
+        <div class="font flex w-full items-center justify-start space-x-2 text-gray-700">
+          <div class="text-lg font-medium underline">This is underline!</div>
+          <div class="text-xxs font-normal text-sky-400">underline</div>
+        </div>
+        <!--  -->
+        <div class="font flex w-full items-center justify-start space-x-2 text-gray-700">
+          <div class="text-lg font-medium line-through">This is line-through!</div>
+          <div class="text-xxs font-normal text-sky-400">line-through</div>
+        </div>
+        <!--  -->
+        <div class="font flex w-full items-center justify-start space-x-2 text-gray-700">
+          <div class="text-lg font-medium no-underline">Text with no underline</div>
+          <div class="text-xxs font-normal text-sky-400">no-underline</div>
+        </div>
+        
        
 </script></code></pre>
 </template>
--- src/pages/typography.vue
+++ src/pages/typography.vue
@@ -51,56 +51,119 @@
             <template v-slot:subHeader
               >Font weight</template
             >
+            <template v-slot:code>
+              <ShowCodeButton
+                v-if="!font_weight"
+                @click.native="font_weight = true"
+              ></ShowCodeButton>
+              <HideCodeButton
+                v-else
+                @click.native="font_weight = false"
+              ></HideCodeButton>
+            </template>
+            <template v-slot:copy
+              ><CopyButton></CopyButton>
+            </template>
           </SubHeading>
           <ComponentPad>
             <template>
               <FontWeightTypo />
             </template>
           </ComponentPad>
-          <!-- code snippet --->          
+          <!-- code snippet --->
+          <span v-show="font_weight" class="transition-all">
           <FontWeightTypoCodeSnippet/>          
-          <!-- end font weight -->
+          </span>          
+          <!-- end code snippet -->
           <!-- Tracking -->
           <SubHeading>
             <template v-slot:subHeader
               >Tracking</template
             >
+            <template v-slot:code>
+              <ShowCodeButton
+                v-if="!tracking_typo"
+                @click.native="tracking_typo = true"
+              ></ShowCodeButton>
+              <HideCodeButton
+                v-else
+                @click.native="tracking_typo = false"
+              ></HideCodeButton>
+            </template>
+            <template v-slot:copy
+              ><CopyButton></CopyButton>
+            </template>
           </SubHeading>
           <ComponentPad>
             <template>
               <TrackingTypography />
             </template>
           </ComponentPad>
-          <!-- code snippet --->          
+          <!-- code snippet --->   
+          <span v-show="tracking_typo" class="transition-all">
           <TrackingTypoCodeSnippet/>          
-          <!-- end font weight -->
+          </span>       
+          <!-- end code snippet -->
           <!-- end Tracking -->
           <!-- Text Decoration  -->
           <SubHeading>
             <template v-slot:subHeader
               >Text Decoration </template
             >
+            <template v-slot:code>
+              <ShowCodeButton
+                v-if="!text_deco"
+                @click.native="text_deco = true"
+              ></ShowCodeButton>
+              <HideCodeButton
+                v-else
+                @click.native="text_deco = false"
+              ></HideCodeButton>
+            </template>
+            <template v-slot:copy
+              ><CopyButton></CopyButton>
+            </template>
           </SubHeading>
           <ComponentPad>
             <template>
               <TextDecoration />
             </template>
           </ComponentPad>
-          <!-- code snippet --->          
+          <!-- code snippet ---> 
+          <span v-show="text_deco" class="transition-all">
           <TextDecorationCodeSnippet/>          
-          <!-- end font weight -->
-          <!-- Text Decoration thickness -->
+          </span>         
+          <!-- end of code snippet -->
           <!-- end Text Decoration  -->
+          <!-- Text Decoration thickness -->
           <SubHeading>
             <template v-slot:subHeader
               >Text Decoration thickness</template
             >
+            <template v-slot:code>
+              <ShowCodeButton
+                v-if="!text_deco_thick"
+                @click.native="text_deco_thick = true"
+              ></ShowCodeButton>
+              <HideCodeButton
+                v-else
+                @click.native="text_deco_thick = false"
+              ></HideCodeButton>
+            </template>
+            <template v-slot:copy
+              ><CopyButton></CopyButton>
+            </template>
           </SubHeading>
           <ComponentPad>
             <template>
               <TextDecorationThickness />
             </template>
           </ComponentPad>
+          <!-- code snippet --->
+          <span v-show="text_deco_thick" class="transition-all">
+          <TextDecorationThicknessCodeSnippet/>          
+          </span>          
+          <!-- end of code snippet -->
           <!-- end Text Decoration thickness -->
          
         </div>
@@ -127,6 +190,10 @@ import TextDecorationThickness from "../components/Typography/TextDecorationThic
 import Heading from "../components/Headers/Heading.vue";
 import SubHeading from "../components/Headers/SubHeading.vue";
 import FontSizeTypoCodeSnippet from "../components/Typography/Code Snippets/FontSizeTypoCodeSnippet.vue"
+import FontWeightTypoCodeSnippet from "../components/Typography/Code Snippets/FontWeightTypoCodeSnippet.vue";
+import TextDecorationThicknessCodeSnippet from "../components/Typography/Code Snippets/TextDecorationThicknessCodeSnippet.vue";
+import TextDecorationCodeSnippet from "../components/Typography/Code Snippets/TextDecorationCodeSnippet.vue";
+import TrackingTypoCodeSnippet from "../components/Typography/Code Snippets/TrackingTypoCodeSnippet.vue";
 
 export default {
   metaInfo: {
@@ -142,11 +209,18 @@ export default {
     TextDecoration,
     TextDecorationThickness,
     FontSizeTypoCodeSnippet,
-    
-  },
+    TextDecorationCodeSnippet,
+    TextDecorationThicknessCodeSnippet,
+    FontWeightTypoCodeSnippet,
+    TrackingTypoCodeSnippet
+},
   data() {
     return {
          font_size: false,
+         font_weight: false,
+         tracking_typo: false,
+         text_deco: false,
+         text_deco_thick: false,
     };
   },
 };
